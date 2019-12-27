7e82a7679099a416e3e19fa2f40d0818af5982dc
imraghav20 <raghav20nov@gmail.com>
2019-12-27 10:13:19 +0530

    removed previous urls and views, created new view and url 'analyze'. Also created a template analyze.html

--- textutils/views.py
+++ textutils/views.py
@@ -5,24 +5,41 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-def removepunc(request):
-    #get the text
+def analyze(request):
+    #Get the text
     djtext = request.GET.get('text', 'default')
-    print (djtext)
-    #analyse the text
-    return HttpResponse('''<h1>remove punctuation</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
-def capfirst(request):
-    return HttpResponse('''<h1>capatalise first</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
+    # Check checkbox values
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
 
-def newlineremove(request):
-    return HttpResponse('''<h1>remove new line</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
 
-def spaceremove(request):
-    return HttpResponse('''<h1>remove space</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button> ''')
+    else:
+        return HttpResponse("Error")
 
-def charcount(request):
-    return HttpResponse('''<h1>count characters</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
+
+#def capfirst(request):
+    #return HttpResponse('''<h1>capatalise first</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
+
+#def newlineremove(request):
+    #return HttpResponse('''<h1>remove new line</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
+
+#def spaceremove(request):
+    #return HttpResponse('''<h1>remove space</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button> ''')
+
+#def charcount(request):
+    #return HttpResponse('''<h1>count characters</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
 
 
