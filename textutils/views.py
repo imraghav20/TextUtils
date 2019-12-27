b1c8179c13f810623f30c33f3913344542135cfe
imraghav20 <raghav20nov@gmail.com>
2019-12-27 08:25:21 +0530

    added buttons to guide user from one page to other

--- textutils/views.py
+++ textutils/views.py
@@ -2,22 +2,26 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>HOME</h1>")
+    return HttpResponse('''<h1>HOME</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/removepunc/"> RemovePunctuation </a></button>
+<br> <button type="button"> <a href="http://127.0.0.1:8000/capitalizefirst/"> CapitaliseFirst </a></button>
+<br> <button type="button"> <a href="http://127.0.0.1:8000/newlineremove/"> NewLineRemove </a></button>
+<br> <button type="button"> <a href="http://127.0.0.1:8000/spaceremove/"> SpaceRemove </a></button>
+<br> <button type="button"> <a href="http://127.0.0.1:8000/charcount/"> CountCharacters </a></button>''')
 
 def removepunc(request):
-    return HttpResponse("remove punctuation")
+    return HttpResponse('''<h1>remove punctuation</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
 def capfirst(request):
-    return HttpResponse("capatalise first")
+    return HttpResponse('''<h1>capatalise first</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
 def newlineremove(request):
-    return HttpResponse("remove new line")
+    return HttpResponse('''<h1>remove new line</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
 def spaceremove(request):
-    return HttpResponse("remove space")
+    return HttpResponse('''<h1>remove space</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button> ''')
 
 def charcount(request):
-    return HttpResponse("count characters")
+    return HttpResponse('''<h1>count characters</h1> <br> <button type="button"> <a href="http://127.0.0.1:8000/"> GoBack </a></button>''')
 
 
 
