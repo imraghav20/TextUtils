11a7c4bdd7e6827e4de1902f8cffaf500912d65b
imraghav20 <raghav20nov@gmail.com>
2019-12-27 08:00:42 +0530

    laid pipeline for website

--- textutils/urls.py
+++ textutils/urls.py
@@ -19,5 +19,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('removepunc/', views.removepunc, name='rempun'),
+    path('capitalizefirst/', views.capfirst, name='capfirst'),
+    path('newlineremove/', views.newlineremove, name='newlineremove'),
+    path('spaceremove/', views.spaceremove, name='spaceremove'),
+    path('charcount/', views.charcount, name='charcount'),
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -1,5 +1,24 @@
 #I have created this file- Abhishek
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<h1>This is your first page.</h1>")
+def home(request):
+    return HttpResponse("<h1>HOME</h1>")
+
+def removepunc(request):
+    return HttpResponse("remove punctuation")
+
+def capfirst(request):
+    return HttpResponse("capatalise first")
+
+def newlineremove(request):
+    return HttpResponse("remove new line")
+
+def spaceremove(request):
+    return HttpResponse("remove space")
+
+def charcount(request):
+    return HttpResponse("count characters")
+
+
+
+
